39318e5c84ae2e2717b8a87acab485fe4ec16f14
David Domínguez Blanco <60604817+DavidDominguez2@users.noreply.github.com>
2023-05-15 11:41:56 +0200

    Add project files.

---
+++ VTChallengeAzure/Services/ServiceStorageBlob.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace VTChallengeAzure.Services {
+    public class ServiceStorageBlob {
+
+        private BlobServiceClient client;
+
+        public ServiceStorageBlob(BlobServiceClient client) {
+            this.client = client;
+        }
+
+        public async Task<string> GetBlobAsync(string containerName, string nameBlob) {
+            BlobContainerClient containerClient = this.client.GetBlobContainerClient(containerName);
+            BlobClient blobClient = containerClient.GetBlobClient(nameBlob);
+            return blobClient.Uri.AbsoluteUri;
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream stream) {
+            BlobContainerClient containerClient = this.client.GetBlobContainerClient(containerName);
+            await containerClient.UploadBlobAsync(blobName, stream);
+        }
+
+        public async Task DeleteBlobAsync(string containerName, string blobName) {
+            BlobContainerClient containerClient = this.client.GetBlobContainerClient(containerName);
+            await containerClient.DeleteBlobAsync(blobName);
+        }
+
+        public async Task<BlobContainerClient> GetContainerAsync(string containerName) {
+            BlobContainerClient container  = this.client.GetBlobContainerClient(containerName);
+            return container;
+        }
+
+    }
+}
---
+++ VTChallengeAzure/Services/ServiceVTChallenge.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using NugetVTChallenge.Models;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+
+namespace VTChallengeAzure.Services {
+    public class ServiceVTChallenge {
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private MediaTypeWithQualityHeaderValue Header;
+        private string UrlApi;
+
+        public ServiceVTChallenge(IConfiguration configuration, IHttpContextAccessor httpContextAccessor) {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiVTChallenge");
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        #region GENERAL
+        private async Task<T> CallApiAsync<T>(string request) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                HttpResponseMessage response = await client.GetAsync(request);
+                return response.IsSuccessStatusCode ? await response.Content.ReadAsAsync<T>() : default(T);
+            }
+        }
+
+        private async Task<HttpStatusCode> InsertApiAsync<T>(string request, T objeto) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                string json = JsonConvert.SerializeObject(objeto);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                return response.StatusCode;
+            }
+        }
+
+        private async Task<HttpStatusCode> UpdateApiAsync<T>(string request, T objeto) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                string json = JsonConvert.SerializeObject(objeto);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+                return response.StatusCode;
+            }
+        }
+
+        // Se supone que en el request ya va el id. Ejemplo: http:/localhost/api/deletealgo/17
+        private async Task<HttpStatusCode> DeleteApiAsync(string request) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+
+                HttpResponseMessage response = await client.DeleteAsync(request);
+                return response.StatusCode;
+            }
+        }
+        #endregion
+
+        #region GENERAL TOKEN
+        private async Task<T> CallApiAsync<T>(string request, string token) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                HttpResponseMessage response = await client.GetAsync(request);
+                return response.IsSuccessStatusCode ? await response.Content.ReadAsAsync<T>() : default(T);
+            }
+        }
+
+        private async Task<HttpStatusCode> InsertApiAsync<T>(string request, T objeto, string token) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                string json = JsonConvert.SerializeObject(objeto);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                return response.StatusCode;
+            }
+        }
+
+        private async Task<HttpStatusCode> UpdateApiAsync<T>(string request, T objeto, string token) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                string json = JsonConvert.SerializeObject(objeto);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+                return response.StatusCode;
+            }
+        }
+
+        // Se supone que en el request ya va el id. Ejemplo: http:/localhost/api/deletealgo/17
+        private async Task<HttpStatusCode> DeleteApiAsync(string request, string token) {
+            using (HttpClient client = new HttpClient()) {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                HttpResponseMessage response = await client.DeleteAsync(request);
+                return response.StatusCode;
+            }
+        }
+        #endregion
+
+        #region TOKEN
+        public async Task<string?> GetToken(string user, string pass) {
+            LogIn model = new LogIn { Name = user, Password = pass };
+            string token = "";
+
+            using (HttpClient client = new HttpClient()) {
+                string request = "/api/auth/login";
+                client.BaseAddress = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string jsonModel = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(jsonModel, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                if (response.IsSuccessStatusCode) {
+                    string data = await response.Content.ReadAsStringAsync();
+                    JObject jsonObject = JObject.Parse(data);
+                    token = jsonObject.GetValue("response").ToString();
+                }
+                return (token != "") ? token : null;
+            }
+        }
+        #endregion
+
+        #region USUARIO
+        public async Task RegisterUser(string name, string password, string image) {
+            Usuario newUser = new Usuario {
+                IdUser = 0,
+                Name = name,
+                Password = password,
+                PasswordSHA = Encoding.ASCII.GetBytes(""),
+                Salt = "",
+                Image = image
+            };
+            string request = "/api/auth/Register";
+            await this.InsertApiAsync<Usuario>(request, newUser);
+        }
+
+        public async Task<Usuario> GetPerfil() {
+            string request = "/api/auth/getperfil";
+            string token = _httpContextAccessor.HttpContext.Session.GetString("TOKEN");
+            return await this.CallApiAsync<Usuario>(request, token);
+        }
+        #endregion
+
+    }
+}
